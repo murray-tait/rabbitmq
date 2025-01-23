cba4c013c9ab85431cb5490188ccdb380a5acfa4
Peter Henderson <peter.henderson@accenture.com>
2025-01-23 18:06:07 +0000

    Separate upstream and downstream modules.

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,87 +29,34 @@ module "rabbit_broker_upstream" {
   subnet_ids = module.vpc.public_subnets
 }
 
-data "aws_secretsmanager_secret_version" "downstream_rabbit_mq_admin" {
-  secret_id = module.rabbit_broker_downstream.rabbit_mq_secret_arn
+module "rabbit_upstream" {
+  source = "./modules/rabbit_upstream"
+  name_base = "RabbitUpstream"
+  subnet_ids = [module.vpc.public_subnets[0]]
+  vhost_name = local.vhost_name
+  queue_name = local.queue_name
+  is_ha = false
+  is_public = true
 }
 
-data "aws_secretsmanager_secret_version" "upstream_rabbit_mq_admin" {
-  secret_id = module.rabbit_broker_upstream.rabbit_mq_secret_arn
-}
+# module "rabbit_downstream" {
+#   source = "./modules/rabbit_downstream"
+#   name_base = "RabbitDownstream"
+#   subnet_ids = [module.vpc.public_subnets[0]]
+#   vhost_name = local.vhost_name
+#   queue_name = local.queue_name
+#   is_ha = false
+#   is_public = true
+#   upstream_broker_amqps_endpoint = module.rabbit_broker_upstream.rabbit_mq_broker_https_endpoint
+#   upstream_rabbit_creds = {
+#     username = module.rabbit_upstream.rabbit_queue_user_creds["username"]
+#     password = module.rabbit_upstream.rabbit_queue_user_creds["password"]
+#   }
+#   upstream_vhost_name = local.vhost_name
+# }
 
-locals {
-  downstream_username = jsondecode("${data.aws_secretsmanager_secret_version.downstream_rabbit_mq_admin.secret_string}")["username"]
-  downstream_password = jsondecode("${data.aws_secretsmanager_secret_version.downstream_rabbit_mq_admin.secret_string}")["password"]
-  upstream_username = jsondecode("${data.aws_secretsmanager_secret_version.upstream_rabbit_mq_admin.secret_string}")["username"]
-  upstream_password = jsondecode("${data.aws_secretsmanager_secret_version.upstream_rabbit_mq_admin.secret_string}")["password"]
-}
 
-provider "rabbitmq" {
-  alias = "downstream"
-  endpoint = module.rabbit_broker_downstream.rabbit_mq_broker_https_endpoint
-  username = local.downstream_username
-  password = local.downstream_password
-}
 
-provider "rabbitmq" {
-  alias = "upstream"
-  endpoint = module.rabbit_broker_upstream.rabbit_mq_broker_https_endpoint
-  username = local.upstream_username
-  password = local.upstream_password
-}
 
-resource "rabbitmq_vhost" "upstream" {
-  provider = rabbitmq.upstream
-  name = local.vhost_name
-}
 
-resource "rabbitmq_vhost" "downstream" {
-  provider = rabbitmq.downstream
-  name = local.vhost_name
-}
 
-locals {
-  upstream_endpoint_amqps_trimmed = trimprefix(module.rabbit_broker_upstream.rabbit_mq_broker_amqps_endpoint, "amqps://")
-  upstream_endpoint_amqps_auth = "amqps://${local.upstream_username}:${local.upstream_password}@${local.upstream_endpoint_amqps_trimmed}/${rabbitmq_vhost.upstream.name}"
-}
-
-resource "rabbitmq_federation_upstream" "this" {
-  provider = rabbitmq.downstream
-  name = "FederationToUpstream"
-  vhost = rabbitmq_vhost.downstream.name
-  definition {
-    uri = local.upstream_endpoint_amqps_auth
-  }
-}
-
-resource "rabbitmq_queue" "upstream" {
-  provider = rabbitmq.upstream
-  name = local.queue_name
-  settings {
-    durable = true
-    auto_delete = false
-  }
-  vhost = rabbitmq_vhost.upstream.name
-}
-
-resource "rabbitmq_queue" "downstream" {
-  provider = rabbitmq.downstream
-  name = local.queue_name
-  settings {
-    durable = true
-    auto_delete = false
-  }
-  vhost = rabbitmq_vhost.downstream.name
-}
-
-resource "rabbitmq_policy" "connect_to_upstream_queue" {
-  provider = rabbitmq.downstream
-  name = "PullFromUpstreamQueue"
-  vhost = rabbitmq_vhost.downstream.name
-  policy {
-    apply_to = "queues"
-    definition = {"federation-upstream-set": "all"}
-    pattern = rabbitmq_queue.upstream.name
-    priority = 10
-  }
-}
